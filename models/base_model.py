2763b628e830c6ee4e0ad01455e942acbd632ac1
wizdom <154702154+owizdom@users.noreply.github.com>
2026-01-11 21:02:48 +0200

    Enhance documentation and initialization logic in BaseModel
    Refactor BaseModel class documentation and methods.

--- models/base_model.py
+++ models/base_model.py
@@ -1,90 +1,117 @@
 #!/usr/bin/python3
-"""_summary_
-    This module cotains the {BaseModel} class - A class that defines
-    all common attributes/methods for other classes
-"""
 
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """_summary_
-        ==> { The BaseModel class } <==
-        id: string - assign with an uuid when an instance is created
-           ** the goal is to have unique id for each BaseModel **
-        created_at: datetime - assign with the current datetime when
-        an instance is created
-        updated_at: datetime - assign with the current datetime when
-        an instance is created and it will be updated every time you
-        change your object
+    """
+    BaseModel class that defines common methods and attributes for other
+    classes.
+
+    This class provides methods for managing unique object identifiers,
+    timestamps for creation and updates, and converting instances into
+    dictionary representations.
+
+    Attributes:
+        id (str): Unique identifier for each instance, generated using UUID.
+        created_at (datetime): Timestamp when the instance was created.
+        updated_at (datetime): Timestamp when the instance was last updated.
+
+    Methods:
+        __str__(): Returns a string representation of the instance.
+        save(): Updates the 'updated_at' timestamp and saves the instance.
+        to_dict(): Converts the instance to a dictionary including all
+                   instance attributes.
     """
 
+    # public instances
     def __init__(self, *args, **kwargs):
-        # I had to import storage inside here because i'll get a
-        # circular importation error if it is at the top level
-        from models import storage
-        """_summary_
-            This is the constructor method for initializing instances
-            of our BaseModel
-            args(tuple): it is a tuple of all positional arguments
-            kwargs(dict): It is a dictionary containing all key-word
-            arguments.
         """
-        if len(kwargs) == 0:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
+        Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list (not used in this class).
+            **kwargs: Dictionary of keyword arguments to populate instance
+                      attributes.
+                - If no kwargs, a new UUID and timestamp are generated.
+                - If kwargs are provided, the instance is populated with the
+                  given values.
+
+        Example:
+            base_instance = BaseModel(id="123",
+            created_at="2025-01-01T00:00:00",
+            updated_at="2025-01-01T01:00:00")
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        if value is None:
-                            raise TypeError
-                        if isinstance(value, str):
-                            setattr(self, key, datetime.fromisoformat(value))
-                        else:
-                            setattr(self, key, value)
-                    elif key == 'id' and value is None:
-                        raise TypeError
+                if key != "__class__":
+                    if key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+                    elif key == "created_at":
+                        setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
 
     def __str__(self):
-        """_summary_
-            Returns the string representation of BaseModel object in
-            this format: [<class name>] (<self.id>) <self.__dict__>
         """
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+        Returns a string representation of the BaseModel instance.
 
-    def save(self):
-        """_summary_
-            save(self): updates the public instance attribute
-            updated_at with the current datetime
+        Returns:
+            str: A formatted string with the class name, instance ID, and a
+                 dictionary of its attributes.
+
+        Example:
+            "[BaseModel] (1234) {'id': '1234',
+            'created_at': '2025-01-01T00:00:00',
+            'updated_at': '2025-01-01T01:00:00'}"
         """
-        # I had to import storage inside here because i'll get a
-        # circular importation error if it is at the top level
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    # public methods
+    def save(self):
+        """
+        Updates the 'updated_at' attribute with the current datetime and saves
+        the instance.
+
+        This method updates the instance's 'updated_at' field and adds the
+        instance to the storage. Calls the 'new' and 'save' methods from the
+        storage module to persist the changes.
+
+        Example:
+            base_instance.save()
+        """
+        self.updated_at = datetime.today()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """_summary_
-            returns a dictionary containing all keys/values of __dict__
-            of the instance:
-
-            - only instance attributes set will be returned
-            - a key __class__ is added with the class name of the
-            object
-            - created_at and updated_at must be converted to string
-            object in ISO format.
         """
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if key in ["created_at", "updated_at"]:
-                value = self.__dict__[key].isoformat()
-                new_dict[key] = value
-        return new_dict
+        Returns a dictionary representation of the instance with all
+        attributes.
+
+        The dictionary includes the 'created_at' and 'updated_at' attributes as
+        ISO format strings and also adds the '__class__' key to indicate the
+        class type.
+
+        Returns:
+            dict: A dictionary of instance attributes including class name and
+                  timestamps.
+
+        Example:
+            {
+                'id': '1234',
+                'created_at': '2025-01-01T00:00:00',
+                'updated_at': '2025-01-01T01:00:00',
+                '__class__': 'BaseModel'
+            }
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["__class__"] = self.__class__.__name__
+        return instance_dict
